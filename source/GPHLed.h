0aa6b3fd9524bb75d235a29e04491a8153ae9fba
Greg Hicks <greghicks@iinet.net.au>
2022-06-14 16:58:58 +1000

    First commit

---
+++ source/GPHLed.h
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+
+#ifndef GPHLedH
+#define GPHLedH
+//---------------------------------------------------------------------------
+#include <SysUtils.hpp>
+#include <Classes.hpp>
+#include <Controls.hpp>
+//---------------------------------------------------------------------------
+class PACKAGE TGPHLed : public TGraphicControl
+{
+private:
+protected:
+public:
+        __fastcall TGPHLed(TComponent* Owner);
+__published:
+};
+//---------------------------------------------------------------------------
+#endif
